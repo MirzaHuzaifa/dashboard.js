71266fe2db77abcb100694ddf3315ff1afa8b0de
huzaifa <mirzahuzaifa309@gmail.com>
2023-07-02 23:49:11 -0700

    showing-dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Dashboard = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex items-center justify-center h-screen">
       <div className="p-6 bg-white rounded shadow-md">
-        <h2 className="text-2xl font-bold mb-6">Welcome to the Dashboard!</h2>
+        <h2 className="mb-6 text-2xl font-bold">Welcome to the Dashboard!</h2>
         <p className="text-gray-700">This is your dashboard content.</p>
+        <h1 className="text-5xl font-bold underline">
+        Hello world!
+      </h1>
         {/* Add more dashboard content here */}
       </div>
     </div>
@@ -13,4 +16,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-``
